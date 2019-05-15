c795bcaf90b03cfcc5810656d5295ce04b1b2b20
Anthonius Munthi <me@itstoni.com>
2019-05-16 01:24:04 +0800

    cs fix

--- api/src/Core/Controller/DefaultController.php
+++ api/src/Core/Controller/DefaultController.php
@@ -14,8 +14,6 @@ declare(strict_types=1);
 namespace SIAP\Core\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
