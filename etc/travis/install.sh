870e090f4c5adbd4df27528833b87c1bf59a0f7d
Anthonius Munthi <me@itstoni.com>
2019-05-18 20:08:57 +0800

    test

--- etc/travis/install.sh
+++ etc/travis/install.sh
@@ -1,11 +1,11 @@
-if [[ $CLIENT != yes || $INTEGRATION == yes ]]; then
+if [[ $CLIENT != yes || $COVERAGE=yes || $INTEGRATION == yes ]]; then
   cp etc/travis/env.local api/.env.local;
   cd $TRAVIS_BUILD_DIR/api;
   composer install --ansi;
   composer prepare-test --ansi;
 fi;
 
-if [[ $CLIENT == yes || $INTEGRATION == yes ]]; then
+if [[ $CLIENT == yes || $COVERAGE=yes || $INTEGRATION == yes ]]; then
   cd $TRAVIS_BUILD_DIR/client;
   yarn install;
 fi;
@@ -14,4 +14,3 @@ if [[ $INTEGRATION == yes || $DEPLOY == yes ]]; then
   cd $TRAVIS_BUILD_DIR/client;
   yarn build;
 fi;
-
