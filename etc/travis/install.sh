edf530cd0313bb3fac12f6b40180bed0d695a34b
Anthonius Munthi <me@itstoni.com>
2019-05-18 20:37:36 +0800

    test

--- etc/travis/install.sh
+++ etc/travis/install.sh
@@ -1,16 +1,22 @@
-if [[ $API = yes || $COVERAGE = yes || $INTEGRATION = yes ]]; then
+if [[ $API = yes || $COVERAGE = yes ]]; then
   cp etc/travis/env.local api/.env.local;
   cd $TRAVIS_BUILD_DIR/api;
   composer install --ansi;
   composer prepare-test --ansi;
 fi;
 
-if [[ $CLIENT = yes || $COVERAGE = yes || $INTEGRATION = yes ]]; then
+if [[ $CLIENT = yes || $COVERAGE = yes ]]; then
   cd $TRAVIS_BUILD_DIR/client;
   yarn;
 fi;
 
-if [[ $INTEGRATION = yes || $DEPLOY = yes ]]; then
+if [[ $DEPLOY = yes ]]; then
   cd $TRAVIS_BUILD_DIR/client;
   yarn build;
+fi;
+
+if [[ $INTEGRATION = yes ]]; then
+  docker-compose pull
+  docker-compose build
+  docker-compose up -d
 fi;
