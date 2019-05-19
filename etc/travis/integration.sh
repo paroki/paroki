2b6dc0b6077ae8751e3a4a86026578428195b6bf
Anthonius Munthi <me@itstoni.com>
2019-05-19 12:42:35 +0800

    test

--- etc/travis/integration.sh
+++ etc/travis/integration.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-service nginx status
-curl http://siap.localhost
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/lib/common.sh"
+
+print_header "Integration" "check nginx status"
+run_command "service nginx status"
+
+print_header "Integration" "check nginx output"
+run_command "curl http://siap.localhost/"
