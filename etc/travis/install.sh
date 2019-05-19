e5921d451b198b81b54f100c1841d364ba873de2
Anthonius Munthi <me@itstoni.com>
2019-05-19 13:00:09 +0800

    test

--- etc/travis/install.sh
+++ etc/travis/install.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
-
 set -e
+
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/lib/common.sh"
 
 code=0
 BUILD_DIR=$PWD
 SITE_DOMAIN=siap.localhost
 
-source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/lib/common.sh"
+if [[ $INTEGRATION = yes ]]; then
+ run_command "cd ${BUILD_DIR}"
+ php-fpm --fpm-config ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default --define cgi.fix_pathinfo=0 -i | grep cgi.fix_pathinfo
+ php-fpm --fpm-config ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default --define cgi.fix_pathinfo=0 && ps -C php-fpm
+ sudo cp etc/travis/config/nginx.conf /etc/nginx/sites-available/$SITE_DOMAIN
+ sudo sed -e "s?%TRAVIS_BUILD_DIR%?$TRAVIS_BUILD_DIR?g" --in-place /etc/nginx/sites-available/$SITE_DOMAIN
+ sudo sed -e "s?%SITE_DOMAIN%?$SITE_DOMAIN?g" --in-place /etc/nginx/sites-available/$SITE_DOMAIN
+ sudo ln -s /etc/nginx/sites-available/$SITE_DOMAIN /etc/nginx/sites-enabled/
+ sudo service nginx start && ps -C nginx
+fi;
 
 if [[ $API = yes || $COVERAGE = yes || $INTEGRATION = yes ]]; then
   cp etc/travis/config/env.local api/.env.local
@@ -23,18 +33,6 @@ fi;
 if [[ $DEPLOY = yes || $INTEGRATION = yes ]]; then
   run_command "cd ${BUILD_DIR}/client"
   yarn build;
-fi;
-
-if [[ $INTEGRATION = yes ]]; then
- run_command "cd ${BUILD_DIR}"
- php-fpm --fpm-config ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default --define cgi.fix_pathinfo=0 -i | grep cgi.fix_pathinfo
- php-fpm --fpm-config ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default --define cgi.fix_pathinfo=0 && ps -C php-fpm
- sudo cp etc/travis/config/nginx.conf /etc/nginx/sites-available/$SITE_DOMAIN
- sudo sed -e "s?%TRAVIS_BUILD_DIR%?$TRAVIS_BUILD_DIR?g" --in-place /etc/nginx/sites-available/$SITE_DOMAIN
- sudo sed -e "s?%SITE_DOMAIN%?$SITE_DOMAIN?g" --in-place /etc/nginx/sites-available/$SITE_DOMAIN
- sudo ln -s /etc/nginx/sites-available/$SITE_DOMAIN /etc/nginx/sites-enabled/
- sudo service nginx start && ps -C nginx
- sudo service nginx reload
 fi;
 
 exit ${code}
