e4cf434637d43268a94e2f6bc1c5862a372f6fcd
Anthonius Munthi <me@itstoni.com>
2019-05-19 14:00:26 +0800

    test

--- etc/travis/install.sh
+++ etc/travis/install.sh
@@ -19,6 +19,10 @@ if [[ $CLIENT = yes || $COVERAGE = yes || $INTEGRATION = yes ]]; then
   yarn;
 fi;
 
+if [[ INTEGRATION = yes ]]; then
+  run_command 'cp etc/travis/config/client.local client/.env.local'
+fi;
+
 if [[ $DEPLOY = yes || $INTEGRATION = yes ]]; then
   run_command "cd ${BUILD_DIR}/client"
   yarn build;
@@ -26,18 +30,17 @@ fi;
 
 if [[ $INTEGRATION = yes ]]; then
  run_command "cd ${BUILD_DIR}"
- chmod 775 -Rvf client/dist
- sudo gpasswd -a www-data travis
- sudo chown travis:www-data -R client/dist
- php-fpm --fpm-config ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default --define cgi.fix_pathinfo=0 -i | grep cgi.fix_pathinfo
- php-fpm --fpm-config ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default --define cgi.fix_pathinfo=0 && ps -C php-fpm
- sudo cp etc/travis/config/nginx.conf /etc/nginx/sites-available/$SITE_DOMAIN
- sudo sed -e "s?%TRAVIS_BUILD_DIR%?$TRAVIS_BUILD_DIR?g" --in-place /etc/nginx/sites-available/$SITE_DOMAIN
- sudo sed -e "s?%SITE_DOMAIN%?$SITE_DOMAIN?g" --in-place /etc/nginx/sites-available/$SITE_DOMAIN
- sudo ln -s /etc/nginx/sites-available/$SITE_DOMAIN /etc/nginx/sites-enabled/
- sudo service nginx status
- sudo service nginx restart
- cp etc/travis/config/client.local client/.env.local
+ run_command "chmod 775 -Rvf client/dist"
+ run_command "sudo gpasswd -a www-data travis"
+ run_command "sudo chown travis:www-data -R client/dist"
+ run_command "php-fpm --fpm-config ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default --define cgi.fix_pathinfo=0 -i | grep cgi.fix_pathinfo"
+ run_command "php-fpm --fpm-config ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default --define cgi.fix_pathinfo=0 && ps -C php-fpm"
+ run_command "sudo cp etc/travis/config/nginx.conf /etc/nginx/sites-available/$SITE_DOMAIN"
+ run_command 'sudo sed -e "s?%TRAVIS_BUILD_DIR%?$TRAVIS_BUILD_DIR?g" --in-place /etc/nginx/sites-available/$SITE_DOMAIN'
+ run_command 'sudo sed -e "s?%SITE_DOMAIN%?$SITE_DOMAIN?g" --in-place /etc/nginx/sites-available/$SITE_DOMAIN'
+ run_command 'sudo ln -s /etc/nginx/sites-available/$SITE_DOMAIN /etc/nginx/sites-enabled/'
+ run_command "sudo service nginx status"
+ run_command 'sudo service nginx restart'
 fi;
 
 exit ${code}
--- etc/travis/integration.sh
+++ etc/travis/integration.sh
@@ -5,17 +5,17 @@ code=0
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/lib/common.sh"
 
 print_header "Integration" "check client dir"
-run_command "ls -l client/dist" || code=1
+run_command_reporting_status "ls -l client/dist" || code=1
 
 print_header "Integration" "check nginx output"
-run_command "curl http://siap.localhost" || code=2
+run_command_reporting_status "curl http://siap.localhost" || code=2
 
 print_header "Integration" "check api docs output"
-run_command "curl http://siap.localhost/api/user.json" || code=3
+run_command_reporting_status "curl http://siap.localhost/api/user.json" || code=3
 
 print_header "Integration" "check nginx config"
-run_command "cat /etc/nginx/sites-enabled/siap.localhost" || code=4
+run_command_reporting_status "cat /etc/nginx/sites-enabled/siap.localhost" || code=4
 
-run_command "sudo cat /var/log/nginx/project_error.log"
+run_command_reporting_status "sudo cat /var/log/nginx/project_error.log"
 
-exit $code
+exit ${code}
